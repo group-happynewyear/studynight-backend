44b0e890967dfa42dff339aa52484231cb008529
Josh <josh910830@gmail.com>
2023-04-23 16:11:07 +0900

    fix: filter library on core stacktrace

--- src/main/kotlin/kr/happynewyear/notification/application/service/ApplicationService.kt
+++ src/main/kotlin/kr/happynewyear/notification/application/service/ApplicationService.kt
@@ -26,7 +26,7 @@ class ApplicationService(
         val head = "[$applicationName] $exceptionType: $exceptionMessage"
         val fullStacktrace = stacktrace.stream().collect(Collectors.joining("\n"))
         val coreStacktrace = stacktrace.stream()
-            .filter { it.startsWith("kr.happynewyear") }.findFirst()
+            .filter { it.startsWith("kr.happynewyear") }.filter{ !it.startsWith("kr.happynewyear.library") }.findFirst()
             .orElseGet { stacktrace[0] }
 
         val channels = channelRepository.findByApplication(applicationName)
